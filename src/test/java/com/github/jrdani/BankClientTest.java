ecb608c7f52177a6cbcad625c105a74d0eacff6b
Daniel Junior <danjun48@gmail.com>
2021-02-08 20:14:15 -0300

    Simulate a client for Bank Service w/ Junit Test

---
+++ src/test/java/com/github/jrdani/BankClientTest.java
@@ -0,0 +1,29 @@
+package com.github.jrdani;
+
+import com.github.jrdani.models.*;
+import io.grpc.*;
+import org.junit.jupiter.api.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class BankClientTest {
+
+    private BankServiceGrpc.BankServiceBlockingStub blockingStub;
+
+    @BeforeAll
+    private void setup() {
+        ManagedChannel channel = ManagedChannelBuilder
+                .forAddress("localhost", 6565)
+                .usePlaintext()
+                .build();
+        this.blockingStub = BankServiceGrpc.newBlockingStub(channel);
+    }
+
+    @Test
+    public void balanceTest() {
+        BalanceCheckRequest balanceCheckRequest = BalanceCheckRequest.newBuilder()
+                .setAccountNumber(5)
+                .build();
+        Balance balance = this.blockingStub.getBalance(balanceCheckRequest);
+        System.out.println("Received: " + balance.getAmount());
+    }
+}
