da2554d7ca3f07eea2992c087082fd60ab5149f8
Daniel Junior <danjun48@gmail.com>
2021-02-13 21:10:36 -0300

    Client Streaming

---
+++ src/main/java/com/github/jrdani/service/CashStreamingRequest.java
@@ -0,0 +1,36 @@
+package com.github.jrdani.service;
+
+import com.github.jrdani.models.*;
+import com.github.jrdani.server.*;
+import io.grpc.stub.*;
+
+public class CashStreamingRequest implements StreamObserver<DepositRequest> {
+
+    private StreamObserver<Balance> balanceStreamObserver;
+    private int accountBalance;
+
+    public CashStreamingRequest(StreamObserver<Balance> balanceStreamObserver) {
+        this.balanceStreamObserver = balanceStreamObserver;
+    }
+
+    @Override
+    public void onNext(DepositRequest depositRequest) {
+        int accountNumber = depositRequest.getAccountNumber();
+        int amount = depositRequest.getAmount();
+        this.accountBalance = AccountDatabase.addBalance(accountNumber, amount);
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+
+    }
+
+    @Override
+    public void onCompleted() {
+        Balance balance = Balance.newBuilder()
+                .setAmount(this.accountBalance)
+                .build();
+        this.balanceStreamObserver.onNext(balance);
+        this.balanceStreamObserver.onCompleted();
+    }
+}
